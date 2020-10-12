9b3a2e8e8d7965b5f6940aee709697c649c33fff
unknown <fachrezaghifary@gmail.com>
2020-10-12 14:16:30 +0700

    create the web

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
 
-function App() {
+// import grasNewsLogo from './assets/image/logo.png';
+import NewsCards from './components/NewsCards/NewsCards';
+
+// import classes from './App.module.css';
+// import useStyles from './styles';
+
+const App = () => {
+  const alanKey = '041cb5706562934b55d80f32c3fe91702e956eca572e1d8b807a3e2338fdd0dc/stage';
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [activeArticle, setActiveArticle] = useState(-1);
+  // const classes = useStyles();
+
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles }) => {
+        if (command === 'newHeadlines') {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === 'highlight') {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        }
+      }
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Gras messenger</h1>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
   );
 }
---
+++ src/components/NewsCards/NewsCard/NewsCard.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect, createRef } from 'react'
+
+import {
+    Card, CardActions, CardActionArea,
+    CardContent, CardMedia, Button, Typography
+} from '@material-ui/core';
+import classNames from 'classnames';
+
+import classes from './NewsCard.module.css';
+
+const NewsCard = ({ article: { description, publishedAt, source, title, url, urlToImage }, i, activeArticle }) => {
+    const [elRefs, setElrefs] = useState([]);
+
+    const scrollToRef = (ref) => window.scroll(0, ref.current.offsetTop - 50);
+
+    useEffect(() => {
+        setElrefs((refs) => Array(20).fill().map((_, j) => refs[j] || createRef()));
+    }, []);
+
+    useEffect(() => {
+        if (i === activeArticle && elRefs[activeArticle]) {
+            scrollToRef(elRefs[activeArticle]);
+        }
+    }, [i, activeArticle, elRefs]);
+
+    return (
+        <Card ref={elRefs[i]} className={classNames(classes.Card, activeArticle === i ? classes.ActiveCard : null)}>
+            <CardActionArea href={url} target="_blank">
+                <CardMedia
+                    className={classes.Media}
+                    image={urlToImage || 'https://www.industry.gov.au/sites/default/files/August%202018/image/news-placeholder-738.png'}
+                />
+
+                <div className={classes.Details}>
+                    <Typography
+                        variant="body2"
+                        color="textSecondary"
+                        component="h4">
+                        {(new Date(publishedAt)).toDateString()}
+                    </Typography>
+
+                    <Typography
+                        variant="body2"
+                        color="textSecondary"
+                        component="h2">
+                        {source.name}
+                    </Typography>
+                </div>
+
+                <Typography
+                    className={classes.Title}
+                    gutterBottom
+                    variant="h5">{title}
+                </Typography>
+
+                <CardContent>
+                    <Typography
+                        variant="body2"
+                        color="textSecondary"
+                        component="p">
+                        {description}
+                    </Typography>
+                </CardContent>
+
+            </CardActionArea>
+            <CardActions className={classes.CardActions}>
+                <Button size="small" color="primary">Learn More</Button>
+                <Typography variant="h5" color="textSecondary">{i + 1}</Typography>
+            </CardActions>
+        </Card>
+    )
+}
+
+export default NewsCard
